77307e442495ca4eb802f9f5016caf5c0cdc5131
Omer Mirza <omer.mirza.556@gmail.com>
2019-02-24 16:43:48 -0600

    Added a helper method to allow for the use of just plugging in a drink object to remove the right amount from the array lists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,6 +24,7 @@ public class Main {
     public static void main(String[] args) {
 
 
+
     }
 
     /**
@@ -166,6 +167,13 @@ public class Main {
                 whippedCream.remove(whippedCream.size() - 1);
             }
         }
+    }
+
+    /**
+     * helper method to allow for just pluging in the drink object
+     */
+    public static void removeFromArrays(Drink theDrink){
+        removeFromArrays(theDrink.returnDrinkIngredient());
     }
 
 
